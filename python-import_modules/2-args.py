d68573252fcdf52be248bb9bfb8903296e9e10aa
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-05 14:28:49 +0000

    Add task 2: dynamic argument list script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,9 @@ from sys import argv
 
 if __name__ == "__main__":
     arg_count = len(argv) - 1
-    print(f"{arg_count} argument{'s' if arg_count != 1 else ''}{':' if arg_count else '.'}")
+    if arg_count == 0:
+        print("0 arguments.")
+    else:
+        print(f"{arg_count} argument{'s' if arg_count != 1 else ''}:")
     for i, arg in enumerate(argv[1:], 1):
         print(f"{i}: {arg}")
