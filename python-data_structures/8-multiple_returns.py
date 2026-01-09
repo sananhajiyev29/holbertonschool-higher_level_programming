ebd3941897807a6a810d840792823eb5d5ff6fd1
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-09 15:17:05 +0000

    Task 8: fix line length, return length and first char

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    """Return a tuple with the length of the sentence and its first character."""
+    """
+    Return a tuple with the length of the sentence
+    and its first character.
+    """
     if len(sentence) == 0:
         first_char = None
     else:
