08eeea79a42c50015d21fbc47524172d05dda377
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-25 17:02:49 +0000

    Task 11: List all shows and their genre IDs including NULL

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Task 11: List all shows and their genre IDs, including shows without genres
+-- Displays tv_shows.title and tv_show_genres.genre_id (NULL if no genre)
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
