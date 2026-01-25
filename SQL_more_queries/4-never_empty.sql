b91c055d845fe04bcbce15728cd9a34e800438cf
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-25 16:16:26 +0000

    Task 4: Create id_not_null table with default id

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Task 4: Create table id_not_null with id default value 1
+-- Create the table id_not_null if it doesn't exist
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
