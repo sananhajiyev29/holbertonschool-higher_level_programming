a6635e6ad631cc6145c0b95a626f267bc8ffd759
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-25 17:07:58 +0000

    Task 15: List all Comedy shows

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- Task 15: List all Comedy shows
+-- Displays titles of shows linked to the Comedy genre
+
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
