085006125674b1eb0c3d739daf9367cf21d0e29c
sananhajiyev29 <12681@holbertonstudents.com>
2026-01-25 16:30:26 +0000

    Task 7: Create cities table with foreign key to states

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Task 7: Create database hbtn_0d_usa and table cities
+-- Creates the database and the table cities if they do not exist
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
