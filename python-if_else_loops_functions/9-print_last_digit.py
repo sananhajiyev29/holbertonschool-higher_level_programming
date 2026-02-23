e765227c982f67fa8637a15a67b4fbc2ffb41afa
sananhajiyev29 <12681holbertonstudents.com>
2026-02-23 19:50:45 +0000

    fix negative case in 9-print_last_digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        last = -(-number % 10)
-    else:
-        last = number % 10
+    last = abs(number) % 10
     print("{}".format(last), end="")
     return last
